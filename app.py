43cd88a18594bd24f84a248af45e802f3ec7e726
Sumit Patidar <sumitpatidar24240@gmail.com>
2021-08-03 11:59:30 +0530

    f

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+ans = {
+        "is_success": True,
+        "user_id": "sumit_patidar_24082000",
+        "odd": [],
+        "even": []
+    }
+
+@app.route("/bfhl")
+def home():
+    return render_template("index.html")
+
+@app.route("/response", methods=['POST'])
+def res():
+    numbers = request.form.get("numbers")
+    lst = []
+    is_success = True
+    for i in numbers.split(" "):
+        try:
+            lst.append(int(i))
+            if(int(i)%2==0):
+                ans["even"].append(int(i))
+            else:
+                ans["odd"].append(int(i))
+        except:
+            is_success = False
+            ans["is_success"] = False
+            lst.append(i)
+    # print(ans)
+    return str(ans)    
+
+app.run(debug=True)
+
+
+
